29233b05de6aa5f4db93872414361a3c9bdacece
tom <tom.m.rose@gmail.com>
2025-11-11 00:03:04 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use eframe::egui;
+
+#[derive(Default)]
+struct MyApp {
+    counter: i32,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello from egui!");
+            if ui.button("Increment").clicked() {
+                self.counter += 1;
+            }
+            ui.label(format!("Counter: {}", self.counter));
+        });
+    }
+}
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My App",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
